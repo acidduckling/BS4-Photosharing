3348c138aea5ee8c01f7c0e5b256fb0c444b84ff
CLayton Wakefield <acidduckling@gmail.com>
2018-06-06 18:12:00 +1000

    init project

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('compile-sass', function() {
+    return gulp
+        .src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('move-js', function() {
+    return gulp
+        .src([
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/tether/dist/js/tether.min.js',
+            'node_modules/jquery/dist/jquery.min.js'
+        ])
+        .pipe(gulp.dest('src/js/'))
+        .pipe(browserSync.stream());
+});
+
+// Run sass when server runs
+// Run server
+// Watch for any changes in src/scss folder and reload the browser
+// Watch for HTML changes
+gulp.task('launch-server', ['compile-sass'], function() {
+    browserSync.init({
+        server: './src/',
+        open: false
+    });
+    gulp.watch(
+        ['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'],
+        ['compile-sass']
+    );
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+// Run gulp
+// Launch server and browser
+// execute JS task
+gulp.task('default', ['move-js', 'launch-server']);

98df71c5217a3fee2d944f608c6117f288b8fc2d
CLayton Wakefield <acidduckling@gmail.com>
2018-06-07 16:30:03 +1000

    Fininsh the site

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,33 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
 gulp.task('compile-sass', function() {
-    return gulp
-        .src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream());
+  return gulp
+    .src([
+      'node_modules/bootstrap/scss/bootstrap.scss',
+      'node_modules/font-awesome/scss/font-awesome.scss',
+      'src/scss/*.scss'
+    ])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('move-js', function() {
-    return gulp
-        .src([
-            'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'node_modules/tether/dist/js/tether.min.js',
-            'node_modules/jquery/dist/jquery.min.js'
-        ])
-        .pipe(gulp.dest('src/js/'))
-        .pipe(browserSync.stream());
+  return gulp
+    .src([
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/tether/dist/js/tether.min.js',
+      'node_modules/jquery/dist/jquery.min.js'
+    ])
+    .pipe(gulp.dest('src/js/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('move-fonts', function() {
+  return gulp
+    .src(['node_modules/font-awesome/fonts/*'])
+    .pipe(gulp.dest('src/fonts'))
+    .pipe(browserSync.stream());
 });
 
 // Run sass when server runs
@@ -26,18 +37,22 @@ gulp.task('move-js', function() {
 // Watch for any changes in src/scss folder and reload the browser
 // Watch for HTML changes
 gulp.task('launch-server', ['compile-sass'], function() {
-    browserSync.init({
-        server: './src/',
-        open: false
-    });
-    gulp.watch(
-        ['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'],
-        ['compile-sass']
-    );
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+  browserSync.init({
+    server: './src/',
+    open: false
+  });
+  gulp.watch(
+    [
+      'node_modules/bootstrap/scss/bootstrap.scss',
+      'node_modules/font-awesome/scss/font-awesome.scss',
+      'src/scss/*.scss'
+    ],
+    ['compile-sass']
+  );
+  gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
 // Run gulp
 // Launch server and browser
 // execute JS task
-gulp.task('default', ['move-js', 'launch-server']);
+gulp.task('default', ['move-js', 'move-fonts', 'launch-server']);
